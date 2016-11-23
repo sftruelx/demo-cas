a8fb702d616d7a453d338accb73d211c77218f3c
truelx <truelx@163.com>
2016-11-23 23:21:02 +0800

    ghfghfghfghfg

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -1,19 +1,20 @@
 package com.example.controller;
 
-import org.springframework.http.HttpRequest;
+import org.jasig.cas.client.authentication.AttributePrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 
 @Controller
 public class HomeController {
     @RequestMapping(value="/home")
     @ResponseBody
-    public String home(HttpServletRequest request){
-//        String user = request.getRemoteUser();
-//        System.out.println(user);
+    public String home(HttpServletRequest request, Principal principal){
+        String user = request.getRemoteUser();
+        System.out.println(user);
         return "index";
     }
 }
